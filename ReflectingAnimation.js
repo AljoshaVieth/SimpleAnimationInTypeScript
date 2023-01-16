6d05bfdb75c71f2936b58b0d6288c86e854f84cf
Aljosha <mail@aljoshavieth.de>
2023-01-16 15:35:51 +0000

    added debug messages in html

--- ReflectingAnimation.js
+++ ReflectingAnimation.js
@@ -1,4 +1,10 @@
 "use strict";
+// Debug elements:
+let xPosDebug = document.getElementById("debugXpos");
+let yPosDebug = document.getElementById("debugYpos");
+let vXDebug = document.getElementById("debugXv");
+let vYDebug = document.getElementById("debugYv");
+let statusMessageDebug = document.getElementById("debugStatusMessage");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const WINDOW_WIDTH = canvas.width;
@@ -14,6 +20,7 @@ let collisionTimestamp = 0;
 let lastTime = new Date().getTime();
 let currentTime = 0;
 let deltaTime = 0;
+let statusMessage = "";
 function draw() {
     window.requestAnimationFrame(draw);
     currentTime = new Date().getTime();
@@ -24,6 +31,17 @@ function draw() {
     currentX += deltaTime * vX;
     currentY += deltaTime * vY;
     console.log(`current x: ${currentX}, current y: ${currentY}`);
+    xPosDebug.textContent = "X-Position:" + Math.round(currentX);
+    yPosDebug.textContent = "Y-Position:" + Math.round(currentY);
+    vXDebug.textContent = "X-Velocity:" + vX;
+    vYDebug.textContent = "Y-Velocity:" + vY;
+    if (vX > 0) {
+        statusMessage = "Moving right...";
+    }
+    else {
+        statusMessage = "Moving left...";
+    }
+    statusMessageDebug.textContent = "Status: " + statusMessage;
     // right border and left border
     if (currentX > WINDOW_WIDTH - diameter / 2 && vX > 0) {
         vX = -vX;
--- ReflectingAnimation.ts
+++ ReflectingAnimation.ts
@@ -1,3 +1,13 @@
+// Debug elements:
+let xPosDebug = document.getElementById("debugXpos") as HTMLParagraphElement;
+let yPosDebug = document.getElementById("debugYpos") as HTMLParagraphElement;
+let vXDebug = document.getElementById("debugXv") as HTMLParagraphElement;
+let vYDebug = document.getElementById("debugYv") as HTMLParagraphElement;
+let statusMessageDebug = document.getElementById("debugStatusMessage") as HTMLParagraphElement;
+
+
+
+
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
 const WINDOW_WIDTH = canvas.width;
@@ -16,6 +26,7 @@ let collisionTimestamp = 0;
 let lastTime = new Date().getTime();
 let currentTime = 0;
 let deltaTime = 0;
+let statusMessage = "";
 
 function draw() {
     window.requestAnimationFrame(draw);
@@ -35,6 +46,19 @@ function draw() {
     currentX += deltaTime * vX;
     currentY += deltaTime * vY;
     console.log(`current x: ${currentX}, current y: ${currentY}`);
+    xPosDebug.textContent = "X-Position:" + Math.round(currentX);
+    yPosDebug.textContent = "Y-Position:" + Math.round(currentY);
+    vXDebug.textContent = "X-Velocity:"+vX;
+    vYDebug.textContent = "Y-Velocity:"+vY;
+    if(vX > 0){
+        statusMessage = "Moving right..."
+    } else {
+        statusMessage = "Moving left..."
+    }
+    statusMessageDebug.textContent = "Status: " + statusMessage;
+
+
+
 
     // right border and left border
     if (currentX > WINDOW_WIDTH - diameter / 2 && vX > 0) {
