ba85127ddedd85350995c9e06ea1773281736007
Aljosha <mail@aljoshavieth.de>
2023-01-16 15:06:37 +0000

    compiled js

--- ReflectingAnimation.js
+++ ReflectingAnimation.js
@@ -1,48 +1,53 @@
 "use strict";
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 const WINDOW_WIDTH = canvas.width;
 const WINDOW_HEIGHT = canvas.height;
 let startX = 20;
-let startY = 0;
-let vX = 1;
-let vY = 1;
-const diameter = 10;
+let startY = 30;
+let vX = 600;
+let vY = 50;
+const diameter = 50;
 let currentX = startX;
 let currentY = startY;
-let collisionTime = 0;
+let collisionTimestamp = 0;
+let lastTime = new Date().getTime();
+let currentTime = 0;
+let deltaTime = 0;
 function draw() {
-    let time = Date.now();
-    ctx.fillStyle = 'lightgray';
+    window.requestAnimationFrame(draw);
+    currentTime = new Date().getTime();
+    deltaTime = (currentTime - lastTime) / 1000;
+    ctx.fillStyle = "lightgray";
     ctx.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-    console.log(`time: ${time}, collisionTime: ${collisionTime}, time - collisionTime = ${time - collisionTime}`);
-    currentX = startX + (time - collisionTime) * vX;
-    currentY = startY + (time - collisionTime) * vY;
+    console.log(`time: ${currentTime}, collisionTime: ${collisionTimestamp}, time - collisionTime = ${currentTime - collisionTimestamp}`);
+    currentX += deltaTime * vX;
+    currentY += deltaTime * vY;
     console.log(`current x: ${currentX}, current y: ${currentY}`);
     // right border and left border
-    if (currentX >= WINDOW_WIDTH - diameter) {
+    if (currentX >= WINDOW_WIDTH - diameter / 2) {
         vX = -vX;
-        startX = WINDOW_WIDTH - diameter;
+        startX = WINDOW_WIDTH - diameter / 2;
         startY = currentY;
-        collisionTime = time;
+        collisionTimestamp = currentTime;
         console.log(`vX = ${vX}`);
         console.log(`startX = ${startX}`);
         console.log(`right border hit`);
     }
-    else if (currentX <= 0) {
+    else if (currentX <= diameter / 2) {
         vX = -vX;
-        startX = 0;
+        startX = diameter / 2;
         startY = currentY;
-        collisionTime = time;
+        collisionTimestamp = currentTime;
         console.log(`vX = ${vX}`);
         console.log(`startX = ${startX}`);
         console.log(`left border hit`);
     }
-    ctx.fillStyle = 'red';
+    ctx.fillStyle = "red";
     ctx.beginPath();
-    //console.log(`Drawing arc: x =  ${currentX}, y =  ${currentY}, diameter =  ${diameter} / 2`)
     ctx.arc(currentX, currentY, diameter / 2, 0, 2 * Math.PI);
     ctx.fill();
+    lastTime = currentTime;
 }
-setInterval(draw, 1000 / 2); // call draw function 60 times per second
+draw();
 //# sourceMappingURL=ReflectingAnimation.js.map
